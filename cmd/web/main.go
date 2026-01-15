78a93434d0fc21b13e8624c9aeb6feabda4bf7cf
BlackMunda <devashishsingh488@gmail.com>
2026-01-15 09:52:07 +0530

    Add MySQL driver and initialize connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    _ "github.com/go-sql-driver/mysql"
+    "database/sql"
     "os"
     "log/slog"
     "flag"
@@ -14,19 +16,41 @@ type application struct {
 func main() {
 
     addr := flag.String("addr", ":8080", "sever starting port")
+    dsn := flag.String("dsn", "web:Dev@2005@/snippetbox?parseTime=true", "MySQL data source name")
     flag.Parse()
 
     logger := slog.New(slog.NewTextHandler(os.Stdout, nil))  //handleroption struct customize
+
+    db, err := openDb(*dsn)
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+    defer db.Close()
 
     app := &application{
         logger : logger,
     }
 
-
     logger.Info("starting server on ", slog.Any("addr", *addr))
 
-    err := http.ListenAndServe(*addr, app.routes())
+    err = http.ListenAndServe(*addr, app.routes())
     logger.Error(err.Error())
     os.Exit(1)
+}
+
+func openDb(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+
+    err = db.Ping()
+    if err != nil {
+        db.Close()
+        return nil, err
+    }
+
+    return db, nil
 }
 
